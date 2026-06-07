54d8e0ac0d6756ca56c433f954f973e82d0f3eb1
ahan-halder <halderahan.2005@gmail.com>
2026-06-07 14:44:13 +0530

    Added gpu configuration

---
+++ compiler/backend/gpu/GPUCodeGen.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "HIR.h"
+
+#include <string>
+
+namespace qfx {
+
+std::string generateCudaSource(const HirModule &hir);
+
+mlir::LogicalResult emitCudaKernel(const HirModule &hir, const std::string &outputPath,
+                                   bool emitPtx);
+
+} // namespace qfx
