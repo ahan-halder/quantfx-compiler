32f7eb6d2272cf95b0354c314ee53bbe02c576a0
ahan-halder <halderahan.2005@gmail.com>
2026-06-19 08:42:30 +0530

    Fix .inc include paths: tablegen outputs to build root, not qfx/IR/ subdir
    The mlir_tablegen() CMake macro generates .inc files into CMAKE_CURRENT_BINARY_DIR
    (build/compiler/mlir/), but QFXDialect.h and QFXDialect.cpp were including them
    with a qfx/IR/ prefix, causing the compiler to look for them at
    build/compiler/mlir/qfx/IR/*.inc which doesn't exist.
    
    Fixed by removing the qfx/IR/ prefix from all .inc includes since
    CMAKE_CURRENT_BINARY_DIR is already on the include path.

--- compiler/mlir/include/qfx/IR/QFXDialect.h
+++ compiler/mlir/include/qfx/IR/QFXDialect.h
@@ -2,11 +2,11 @@
 
 #include "mlir/IR/Dialect.h"
 
-#include "qfx/IR/QFXDialect.h.inc"
+#include "QFXDialect.h.inc"
 
 namespace qfx {
 class QFXDialect;
 } // namespace qfx
 
 #define GET_OP_CLASSES
-#include "qfx/IR/QFXOps.h.inc"
+#include "QFXOps.h.inc"
--- compiler/mlir/lib/Dialect/QFXDialect.cpp
+++ compiler/mlir/lib/Dialect/QFXDialect.cpp
@@ -3,17 +3,17 @@
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/DialectImplementation.h"
 
-#include "qfx/IR/QFXDialect.cpp.inc"
+#include "QFXDialect.cpp.inc"
 
 #define GET_OP_CLASSES
-#include "qfx/IR/QFXOps.cpp.inc"
+#include "QFXOps.cpp.inc"
 
 namespace qfx {
 
 void QFXDialect::initialize() {
   addOperations<
 #define GET_OP_LIST
-#include "qfx/IR/QFXOps.cpp.inc"
+#include "QFXOps.cpp.inc"
       >();
 }
 
